77e1a3a2f0bcdaecd24df9da4ef5afae0cdd6d6b
thiagoreisb <thiagoreisb@gmail.com>
2019-06-05 23:51:46 -0300

    Setting up firebase config

--- monitoring_ui/src/login.js
+++ monitoring_ui/src/login.js
@@ -1,5 +1,25 @@
+/* eslint-disable */
+/// Vue
 import Vue from 'vue'
 import Login from './Login.vue'
+
+/// Firebase
+var firebase = require('firebase');
+//var firebase = require('firebase/app');
+var firebaseui = require('firebaseui');
+
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FB_APIKEY,
+  authDomain: process.env.VUE_APP_FB_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DATABASEURL,
+  projectId: process.env.VUE_APP_FB_PROJECTID,
+  storageBucket: process.env.VUE_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FB_APPID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
