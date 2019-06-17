4f7c6281f9fba8077604879cffd7f5275d9e8dff
thiagoreisb <thiagoreisb@gmail.com>
2019-06-16 22:13:58 -0300

    Adding get method

--- web/src/controllers/apiController.js
+++ web/src/controllers/apiController.js
@@ -6,6 +6,19 @@ class ApiController {
     constructor() {
         this.api_base = process.env.VUE_APP_FB_API_BASE;
     }
+
+    /**
+     * Returns the response of GET resource
+     * @param {string} resource API resource
+     * @param {function} cb Callback function
+     * @param {function} err Callback error function
+     */
+    get(resource, cb, err) {
+        axios.get(this.api_base + resource)
+            .then((response) => cb(response.data))
+            .catch((error) => err(error));
+    }
+
 }
 
 const api = new ApiController();
